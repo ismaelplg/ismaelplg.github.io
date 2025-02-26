09ba718eaef455fe3afc1db675319c43daa398f9
Ismael Lopez <ismaelplg@hotmail.com>
2025-02-26 16:51:00 -0600

    feat: add routes and routes animations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./layout/layout.component'),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/home/home.component'),
+      },
+      {
+        path: 'work',
+        loadComponent: () => import('./pages/work/work.component'),
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
