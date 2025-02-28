6c974eba1b4d22dc935e136690a72f492447fb8f
Ismael Lopez <ismaelplg@hotmail.com>
2025-02-28 15:46:52 -0600

    feat: add home page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './home.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class HomeComponent {}
