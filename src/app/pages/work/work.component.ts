a3798ad10b5e4b1195f68eb2a2b394d75d6cdd47
Ismael Lopez <ismaelplg@hotmail.com>
2025-02-28 15:47:03 -0600

    feat: add work page

---
+++ src/app/pages/work/work.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work',
+  imports: [],
+  templateUrl: './work.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class WorkComponent {}
